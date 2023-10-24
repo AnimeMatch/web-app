9bdba0a36d2cb1ab3749acc887a6a35f36a6ffa7
andresPOliveira <andres.oliveira@sptech.school>
2023-10-24 01:01:22 -0300

    modal de cadastro v0

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import '../assets/css/navbar.css';
 import logo from '../assets/images/logos/logoNavbar.png';
-import imgLogin from '../assets/images/backgrounds/4258797 1.svg' 
+import imgLogin from '../assets/images/backgrounds/4258797 1.svg';
+import imgRegister from '../assets/images/backgrounds/4302417 1.svg'; 
 import facebookLogo from '../assets/images/logos/facebook 1.svg';
 import googleLogo from '../assets/images/logos/search 1.svg';
 import twitterLogo from '../assets/images/logos/twitter 1.svg';
@@ -91,13 +92,42 @@ export default function Navbar(){
             }
             {modal2 &&
                 <div className="modal-register">
-                    <div className="overlay-modal" onClick={regsiterModal}></div>
-                    <div className="modal-content register">
-                        <h1>Register</h1>
-                        <span>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, qui.
-                        </span>
-                        <button className="close" onClick={regsiterModal}>fechar</button>
+                     <div className="overlay-modal" onClick={regsiterModal}></div>
+                    <div className="modal-content login">
+                        <img src={imgRegister} alt="" id='loginImg'/>
+                        <div className='modal-right-content'>
+                            <span className="close" onClick={regsiterModal}>X</span>
+                            <h1>Cadastro</h1>
+                            <form action="" method="post" className='form'>
+                                <label htmlFor="email">E-mail</label>
+                                <input type="email" name="" id="" placeholder='email@example.com'/>
+                                <label htmlFor="password">Nome de usuario</label>
+                                <input type="password" name="" id=""placeholder='**************'/>
+                                <label htmlFor="password">Senha</label>
+                                <input type="password" name="" id=""placeholder='**************'/>
+                                <label htmlFor="password">Confirme sua Senha</label>
+                                <input type="password" name="" id=""placeholder='**************'/>
+                                <div className='underLabel'>
+                                    <div className="remember">
+                                        <input type="checkbox" name="" id="" />
+                                        <span className='tinyText'>Sim, compreendo e concordo com os Termos de Serviços e Política de Privacidade do Anime Match.</span>
+                                    </div>
+                                </div>
+                                <button className='btn-secundary'> Cadastrar </button>
+                                <div className="separate"></div>
+                                <div className="register">
+                                    <span id='topRegister'>Ou se conectar com</span>
+                                    <div className="socialLogos">
+                                        <img src={googleLogo} id='google' alt="google" />
+                                        <img src={facebookLogo} id='facebook' alt="facebook" />
+                                        <img src={twitterLogo} id='twitter' alt="twitter" />
+                                    </div>
+                                    <span id='botRegister'>Não tem conta ? <a id='registerBtn'
+                                    onClick={swap}>Cadastre-se</a></span>
+
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             }
