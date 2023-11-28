fd7dd0eff286bf7aa4d50425c0cdb250afec5115
andresPOliveira <andres.oliveira@sptech.school>
2023-11-28 16:06:06 -0300

    ajustes nos index

--- src/components/CardListCustomized.jsx
+++ src/components/CardListCustomized.jsx
@@ -4,74 +4,19 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function CardListCustomizad() {
-  const [getList, setList] = useState([]);
-  const [image0, setImage0] = useState("");
-  const [image1, setImage1] = useState("");
-  const [image2, setImage2] = useState("");
-  const [image3, setImage3] = useState("");
-  const [image4, setImage4] = useState("");
-  
+  const [getList2, setList2] = useState([]);
+
   useEffect(() => {
     apiUser
       .get(`/anime-lista/animes-da-lista?listaId=2`)
       .then((response) => {
         console.log(response.data);
-        setList(response.data);
+        setList2(response.data);
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
-
-
-  const setImages = ()=> {
-    setImage0(getList[0].imagem);
-    setImage1(getList[1].imagem);
-    setImage2(getList[2].imagem);
-    setImage3(getList[3].imagem);
-    setImage4(getList[0].imagem);
-  }
-
-  useEffect(() => {
-    if (getList.length > 0) {
-      setImages();
-       stop
-    }
-  }, [getList]);
-
-
-  const cardStyle = {
-    style0: {
-      backgroundImage: `url(${image0})`,
-      backgroundSize: "cover",
-      backgroundPosition: "center",
-      backgroundRepeat: "no-repeat",
-    },
-    style1: {
-      backgroundImage: `url(${image1})`,
-      backgroundSize: "cover",
-      backgroundPosition: "center",
-      backgroundRepeat: "no-repeat",
-    },
-    style2: {
-      backgroundImage: `url(${image2})`,
-      backgroundSize: "cover",
-      backgroundPosition: "center",
-      backgroundRepeat: "no-repeat",
-    },
-    style3: {
-      backgroundImage: `url(${image3})`,
-      backgroundSize: "cover",
-      backgroundPosition: "center",
-      backgroundRepeat: "no-repeat",
-    },
-    style4: {
-      backgroundImage: `url(${image4})`,
-      backgroundSize: "cover",
-      backgroundPosition: "center",
-      backgroundRepeat: "no-repeat",
-    },
-  };
 
   return (
     <>
@@ -83,13 +28,52 @@ export default function CardListCustomizad() {
           </div>
           <div className="add-custom"></div>
         </div>
-          <div className="card-list">
-            <div className="nth-small" style={cardStyle.style0}></div>
-            <div className="nth-mid" style={cardStyle.style1}></div>
-            <div className="nth-big" style={cardStyle.style2}></div>
-            <div className="nth-mid" style={cardStyle.style3}></div>
-            <div className="nth-small" style={cardStyle.style4}></div>
-          </div>
+        <div className="card-list">
+          {getList2.map((anime, index) => (
+            <>
+              <div
+                key={0}
+                className={index === 0 ? "nth-small" : ""}
+                style={{
+                  backgroundImage: `url(${anime.imagem})`,
+                  backgroundSize: `cover`,
+                }}
+              ></div>
+              <div
+                key={1}
+                className={index === 1 ? "nth-mid" : ""}
+                style={{
+                  backgroundImage: `url(${anime.imagem})`,
+                  backgroundSize: `cover`,
+                }}
+              ></div>
+              <div
+                key={2}
+                className={index === 2 ? "nth-big" : ""}
+                style={{
+                  backgroundImage: `url(${anime.imagem})`,
+                  backgroundSize: `cover`,
+                }}
+              ></div>
+              <div
+                key={3}
+                className={index === 3 ? "nth-mid" : ""}
+                style={{
+                  backgroundImage: `url(${anime.imagem})`,
+                  backgroundSize: `cover`,
+                }}
+              ></div>
+              <div
+                key={4}
+                className={index === 4 ? "nth-small" : ""}
+                style={{
+                  backgroundImage: `url(${anime.imagem})`,
+                  backgroundSize: `cover`,
+                }}
+              ></div>
+            </>
+          ))}
+        </div>
         <div className="line"></div>
         <div className="profile-list">
           <div className="user">
