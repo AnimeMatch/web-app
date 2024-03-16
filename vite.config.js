bfc1f1f61e4e20b5b479dd7bed4bd33d709198a8
lucas.lrodrigues@sptech.school <lucas.lrodrigues@sptech.school>
2024-03-16 20:51:57 -0300

    configurando cors

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    cors: false,
     proxy: {
-      "*": "http://localhost:5173",
-    }  
-  } ,
-  plugins: [react()],
-})
+      '/api': {
+        target: 'http://localhost:5173',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+          'Access-Control-Allow-Headers': 'Content-Type',
+        },
+      },
+    },
+  },
+});
