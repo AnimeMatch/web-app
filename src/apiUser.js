790d91be9492182dc3530e60e8d8fa15dd66ad52
lucas.lrodrigues@sptech.school <lucas.lrodrigues@sptech.school>
2024-03-16 01:53:27 -0300

    melhorando as variaveis

--- src/apiUser.js
+++ src/apiUser.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import config from "./config";
 
 const apiUser = axios.create({
-  baseURL: `http://${config.VITE_URL_ANILIST}/`
+  baseURL: `${config.VITE_URL_ANILIST}/`
 });
 
 export default apiUser;
