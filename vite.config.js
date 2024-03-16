c8a2f585e54eb903f9431ee94c2a27dbb078c447
lucas.lrodrigues@sptech.school <lucas.lrodrigues@sptech.school>
2024-03-16 19:27:49 -0300

    tentando utilizar o cors

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    cors: false,
+    proxy: {
+      '/random_joke': {
+        target: 'https://official-joke-api.appspot.com/',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
 })
