1c75e7e488a45fe49e0813ff100322f073c3cf6f
lucas.lrodrigues@sptech.school <lucas.lrodrigues@sptech.school>
2024-03-16 21:49:05 -0300

    ajustando prefixo

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
+      '*': {
         target: 'http://localhost:5173',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\//, ''),
         headers: {
           'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
