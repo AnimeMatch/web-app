48f7bd73cd4f261aa601b98b7192a7900ad78379
lucas.lrodrigues@sptech.school <lucas.lrodrigues@sptech.school>
2024-03-16 23:08:54 -0300

    cors e config de ambiente

--- src/config.js
+++ src/config.js
@@ -1,18 +1,28 @@
+import { getOS } from './getOS';
+
 const dev = {
     VITE_URL_ANILIST: "http://localhost:8080",
     VITE_URL_DATABASE: "http://localhost:8081"
   };
   
   const prod = {
-    VITE_URL_ANILIST: "http://10.0.0.160:8080",
-    VITE_URL_DATABASE: "http://10.0.0.160:8081"
+    VITE_URL_ANILIST: `http://${import.meta.env.VITE_SERVER_IP}:8080`,
+    VITE_URL_DATABASE: `http://${import.meta.env.VITE_SERVER_IP}:8081`
   };
   
-  const config = {
+  var config = {
     // Add common config values here
     MAX_ATTACHMENT_SIZE: 5000000,
     // Default to dev if not set
     ...(import.meta.env.VITE_ENV === "prod" ? prod : dev),
   };
+  
+  if (!import.meta.env.VITE_ENV){
+    const os = getOS();
+    config = {
+      MAX_ATTACHMENT_SIZE: 5000000,
+      ...(os === "Linux" ? prod : dev),
+    };
+  }
   
   export default config;
