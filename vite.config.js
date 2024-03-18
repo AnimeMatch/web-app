1d9b64555efcb0fe03148f3cd127c52582d89a8b
lucas.lrodrigues@sptech.school <lucas.lrodrigues@sptech.school>
2024-03-17 23:41:41 -0300

    mudando cors

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,14 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: `http://10.0.0.214:8080/`,
+        target: `http://10.0.0.214:8080`,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
       '/apo': {
         target: `http://10.0.0.214:8081/`,
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api2/, ''),
+        rewrite: (path) => path.replace(/^\/apo/, ''),
       },
     },
     cors: true,
