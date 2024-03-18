5a6e8a6330c165662aa1ee901183c0e42b24c531
lucas.lrodrigues@sptech.school <lucas.lrodrigues@sptech.school>
2024-03-17 22:43:34 -0300

    mergeando

--- src/apiUser.js
+++ src/apiUser.js
@@ -2,12 +2,8 @@ import axios from "axios";
 import config from "./config";
 
 const apiUser = axios.create({
-<<<<<<< HEAD
   // baseURL: `${config.VITE_URL_ANILIST}/`
   baseURL: "/api2"
-=======
-  baseURL: `${config.VITE_URL_DATABASE}/`
->>>>>>> b29aafdc768bca6e95e4b346968cb24fa60a4e89
 });
 
 export default apiUser;
