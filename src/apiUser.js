d68f088fca8405bccc47367fe12c9c45792c7af7
lucas.lrodrigues@sptech.school <lucas.lrodrigues@sptech.school>
2024-03-18 02:01:01 -0300

    ajustando para a nova regra de proxy

--- src/apiUser.js
+++ src/apiUser.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import config from "./config";
+// import config from "./config";
 
 const apiUser = axios.create({
   // baseURL: `${config.VITE_URL_ANILIST}/`
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// import config from "./src/config";
+
+// DESENVOLVIMENTO
+const VITE_HOST = "127.0.0.1"
+// PRODUÇÃO
+// const VITE_HOST = "10.0.0.214"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://10.0.0.214:8081',
+        target: `http://${VITE_HOST}:8081`,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
       '/apo': {
-        target: 'http://10.0.0.214:8080',
+        target: `http://${VITE_HOST}:8080`,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/apo/, ''),
       }
