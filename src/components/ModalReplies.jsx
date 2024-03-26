af2019aee845015ca8ba03b5eccf5fd71d91ff9b
andresPOliveira <andres.oliveira@sptech.school>
2024-03-26 16:38:03 -0300

    respostas

---
+++ src/components/ModalReplies.jsx
@@ -0,0 +1,136 @@
+import like from "../assets/images/deafault/gostar(1) 1.svg";
+import deslike from "../assets/images/deafault/desgostar(1) 1.svg";
+import chat from "../assets/images/deafault/bate-papo 2.svg";
+import "../assets/css/commentsWithReplies.css";
+import apiUser from "../apiUser";
+import { useEffect, useState } from "react";
+
+export default function ModalReplies(props) {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    profileImage: "",
+    coverImage: "",
+    criacao: "",
+    genero: "",
+    bio: "",
+  });
+
+  const [replies, setReplies] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await apiUser.get(
+          `/users/user?email=${sessionStorage.email}`
+        );
+
+        const reponse2 = await apiUser.get(
+          `/comentarios-animes/${props.id}/lista_comentarios_filhos`
+        );
+        setReplies(reponse2.data);
+        setUser(response.data);
+      } catch {}
+    };
+    fetchData();
+  }, []);
+
+  const handleLike = () => {
+    apiUser.patch(`/comentarios-animes/like/${props.id}`).then((response) => {
+      if (response.status == 200) {
+        props.reload();
+      }
+    });
+  };
+
+  const handleDeslike = () => {
+    apiUser
+      .patch(`/comentarios-animes/deslike/${props.id}`)
+      .then((response) => {
+        if (response.status == 200) {
+          props.reload();
+        }
+      });
+  };
+
+  const handleState = () => {
+    props.changeState();
+  };
+
+  const sendResponse = () => {
+    apiUser
+      .post(`/comentarios-animes/${props.id}/comentar_comentario`, {
+        texto: document.getElementById("comment_reponse_ipt").value,
+        dataComentarioAnime: new Date(),
+        emailUsuario: sessionStorage.email,
+      })
+      .then((response) => {
+        console.log(response);
+      });
+  };
+
+  return (
+    <>
+      {props.state && (
+        <div className="modal-content-replies">
+          <div className="overlay-modal-content-replies"></div>
+          <div className="content-replies">
+            <div className="info-user">
+              <div className="info-user-left">
+                <div className="circle-image">
+                  <img src={props.image} alt="" />
+                </div>
+                <span className="">{props.name}</span>
+              </div>
+              <div className="infor-user-right">
+                <span onClick={handleState}>X</span>
+              </div>
+            </div>
+            <div className="main-comment">
+              <div className="separation-line"></div>
+              <div className="main-comment-text">
+                <span>{props.text}</span>
+              </div>
+              <div className="separation-line"></div>
+              <div className="comment-reviews">
+                <div className="comment-like" onClick={handleLike}>
+                  <img src={like} alt="" />
+                  <span className="comment-like-total">{props.liked}</span>
+                </div>
+                <div className="comment-deslike" onClick={handleDeslike}>
+                  <img src={deslike} alt="" />
+                  <span className="comment-deslike-total">
+                    {props.desliked}
+                  </span>
+                </div>
+                <div className="comment-childrens">
+                  <img src={chat} alt="" />
+                  <span className="comment-childrens-total">
+                    {props.replies}
+                  </span>
+                </div>
+              </div>
+              <div className="separation-line"></div>
+              <div className="comment-reponse-area">
+                <div className="circle-image">
+                  <img src={user.profileImage} alt="" />
+                </div>
+                <input
+                  type="textarea"
+                  id="comment_reponse_ipt"
+                  placeholder="Postar sua resposta"
+                />
+                <button className="btn-secundary" onClick={sendResponse}>
+                  Reponder
+                </button>
+              </div>
+            </div>
+            <div className="replies-area">
+              <span>Respostas:</span>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
--- src/pages/Info/components/CommentsWithReplies.jsx
+++
@@ -1,6 +0,0 @@
-export default function CommentsWithReplies (){
-    return (
-        <>
-        </>
-    )
-}
