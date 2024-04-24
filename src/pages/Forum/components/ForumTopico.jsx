de328ac378598ad5efa8788f60fed9d7bd87b075
lucas.lrodrigues@sptech.school <lucas.lrodrigues@sptech.school>
2024-04-24 06:43:18 -0300

    atualizando repo remoto

--- src/pages/Forum/components/ForumTopico.jsx
+++ src/pages/Forum/components/ForumTopico.jsx
@@ -6,8 +6,14 @@ export default function Topico (props) {
                 <div className="user-image-topico"
                   style={{background: (`url("${props.userImage}")`), 
                   backgroundSize: "cover"}} />
+                  <div className="topico-texts">
+                    <span className="user-name-topico">{props.userName}</span>
+                    <span className="content-text-topico">{props.topicoContent}</span>
+                  </div>
             </div>
-            
+            <div className="image-topico">
+                
+            </div>
         </div>
         </>
     );  
